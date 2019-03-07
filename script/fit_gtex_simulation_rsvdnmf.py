c4bbbae2b2927c4c23484c5584caeb314b2d1be1
zihao12 <wangzh@uchicago.edu>
2019-03-07 10:08:06 -0600

    rsvdnmf-rnmf-nmf comparison

---
+++ script/fit_gtex_simulation_rsvdnmf.py
@@ -0,0 +1,115 @@
+
+# SCRIPT SETTINGS
+# ---------------
+# These variables specify the names of the input files.
+data_dir           = "../../topics-simulation-bigdata/output/"
+read_counts_file   = "gtex_simulation_nnlm.csv"
+#init_factors_file  = "gtex_simulation_rough_factors.csv"
+#init_loadings_file = "gtex_simulation_rough_loadings.csv"
+
+# These variables specify the names of the output files.
+out_dir           = "../../topics-simulation-bigdata/output/"
+factors_out_file  = "gtex_simulation_factors_rsvdnmf.csv"
+loadings_out_file  = "gtex_simulation_loadings_rsvdnmf.csv"
+
+ # # ## only for testing
+#read_counts_file   = "test.csv"
+#init_factors_file  = "test_factors.csv"
+#init_loadings_file = "test_loadings.csv"
+#factors_out_file  = "test_simulation_factors_rnmfhals.csv"
+#loadings_out_file  = "test_simulation_loadings_rnmfhals.csv"
+##
+
+# SET UP ENVIRONMENT
+# ------------------
+# Load packages and function definitions.
+import numpy as np
+import os
+import sys
+import time
+sys.path.insert(0,'../code/')
+from utility import compute_loglik
+from utility import compute_least_sqr_loss
+sys.path.insert(0,'../myRistretto/')
+from mynmf import compute_nmf
+from mynmf import compute_rnmf
+from mysvd import compute_rsvd
+from scipy.stats import poisson
+
+# LOAD GTEX DATA
+# ------------------
+print("Loading GTEx data.")
+counts = np.loadtxt(open(data_dir + read_counts_file, "rb"), delimiter=",", skiprows=0)
+#print("data shape: p = " + str(X.shape[0], "  n = " str(X.shape[1])))
+print("data shape after transposing:")
+print(counts.shape)
+## p = 55863
+## n = 11688
+## k = 20
+
+
+
+# LOAD INITIAL ESTIMATES
+# ----------------------
+
+## loading is [p, K]
+
+
+# Get the number of factors ("topics").
+K = 20
+
+# RUN RNMFHALS OPTIMIZATION METHOD
+# ---------------------------
+MAX_ITER = 1000
+
+print("start fitting")
+start = time.time()
+np.random.seed(12345)
+U,s,Vt = compute_rsvd(counts.T, rank=K+20, oversample=20, n_subspace=2, n_blocks=1, sparse=False, random_state=1234)
+Xhat = U.dot(np.diag(s)).dot(Vt)
+print("number of 0s in rsvd:" + str(Xhat[Xhat <0].shape[0]))
+Xhat[Xhat <0] = 0
+A,W = compute_nmf(Xhat,rank=K,init = 'nndsvd',random_state = 0,tol=1e-10, maxiter= MAX_ITER, verbose = 1)
+runtime = time.time() - start
+print("finish fitting after: " + str(runtime))
+
+print("save file")
+np.savetxt(out_dir+factors_out_file, A, delimiter=",")
+np.savetxt(out_dir+loadings_out_file, W.T, delimiter=",")
+
+# compute loglikelihood 
+print("compute least square")
+
+#out = compute_loglik(counts.T,A,W)
+lsq = compute_least_sqr_loss(counts.T, A.dot(W)) 
+print("square error             : " + str(lsq))
+
+# A_nmf, W_nmf = poisson2multinom(A_nmf, W_nmf)
+
+# mn_ll = loglik_multinom(X,A_nmf, W_nmf)
+#print("type: " + out["type"])
+#print("poisson loglikelihood	: " + str(out["poisson_ll"]))
+#print("poisson (+eps) loglikelihood	: " + str(out["poisson_ll_eps"]))
+#print("multinomial loglikelihood: " + str(out["multinom_ll"]))
+
+## write skdlda files to file
+#np.savetxt(out_dir+factors_out_file, A, delimiter=",")
+#np.savetxt(out_dir+loadings_out_file, W.T, delimiter=",")
+# with open(out_dir + result_out_file, "wb") as f:
+# 	pickle.dump(result, f)
+## session info
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
